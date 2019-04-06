8ab4d7fca4d8dbc607cd3dc2226d1e28f543acbe
Chris Hallinan <christopherhallinan1@gmail.com>
2019-04-06 06:04:57 +0000

    added a function to do the rotation cipher

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
-int encriptionRotation(int message, int rotationAmount);
+void encriptionRotation(char *message, int rotationAmount);
 
 int main() {
     int choice; //declare a vairiable for the choice of the user
-    int messageText;
-    int rotaionAmount;
+    char messageText[200];
+    int rotationAmount;
     printf("what would you like to do\n1: Encription of rotation\n2: Decription with key of rotation\n");
-    printf("what would you like to do\n3: Encription of substitution\n4: Decription with key of substitution\n");
-    printf("what would you like to do\n5: Decription without key of rotation\n6: Decription without key of substitution");
+    printf("3: Encription of substitution\n4: Decription with key of substitution\n");
+    printf("5: Decription without key of rotation\n6: Decription without key of substitution\n");
     scanf("%d", &choice);
     switch(choice) {
         case 1:
-            printf("enter your message text");
-            scanf("%d", &messageText);
-            printf("enter your rotation amount");
+            printf("enter your message text:\n");
+            scanf("%s", messageText);
+            printf("enter your rotation amount:\n");
             scanf("%d", &rotationAmount);
-            printf("%d", encriptionRotation()) //run encription of rotation function
+            encriptionRotation(messageText, rotationAmount); //run encription of rotation function
+            break;
         case 2:
             //run decription of rotation function
         case 3:
@@ -28,14 +30,20 @@ int main() {
         case 6:
             //run decription without key of substitution function
         default:
-            printf("please enter a choice between 1 and 6");
+            printf("please enter a choice between 1 and 6\n");
             //re ask for a choice from the user
     }
     return 0;
 }
 
-int encriptionRotation(int message, int rotationAmount) {
-    int encripted
-    
-    return encripted;
+void encriptionRotation(char *message, int rotationAmount) {
+    char encripted;
+    int encriptedValue;
+    int n;
+    for (n = 0 ; message[n] != '\0' && strlen(message) > n ; n++) {
+		encriptedValue = ((int)message[n] -97 + rotationAmount) % 26 + 97;
+		encripted = (char)(encriptedValue);
+		printf("%c", encripted);
+	}
+	printf("\n");
 }
